44c72aef9994adffc2194863d250da2a69ff581e
boburDev <negmatov1907@gmail.com>
2021-02-08 20:18:25 +0500

    add post model and home table view

--- instaTimeLine/controllers/HomeViewController.swift
+++ instaTimeLine/controllers/HomeViewController.swift
@@ -1,22 +1,56 @@
-//
-//  HomeViewController.swift
-//  instaTimeLine
-//
-//  Created by Boburmirzo on 2/7/21.
-//
-
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableHomeView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initViews()
     }
 
 
     
     // MARK: - Method
-
+    
+    func initViews() {
+        tableHomeView.dataSource = self
+        tableHomeView.delegate = self
+        
+        
+        setNavigationBar()
+    }
+    
+    func setNavigationBar() {
+        
+        let camera = UIImage(named: "ic_camera")
+        let send = UIImage(named: "ic_send")
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: camera, style: .plain, target: self, action: #selector(leftBtnOnPress))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: send, style: .plain, target: self, action: #selector(rightBtnOnPress))
+        
+        title = "Instagram"
+    }
+    
+    // MARK: - Action
+    
+    @objc func leftBtnOnPress(){
+        
+    }
+    
+    @objc func rightBtnOnPress(){
+        
+    }
+    
+    // MARK: - Table View
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
---
+++ instaTimeLine/model/Post.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class Post {
+    var fullname: String = ""
+    var user_img: String = ""
+    var post_img: String = ""
+    
+    init(fullname: String, user_img: String, post_img: String) {
+        self.fullname = fullname
+        self.user_img = user_img
+        self.post_img = post_img
+    }
+}
